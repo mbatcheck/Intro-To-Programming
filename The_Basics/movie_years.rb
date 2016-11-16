e58f7eb80820f6e63ae449af9aa2bccfdcb15eae
Mark Batcheck <mark@fourwindsintl.com>
2016-11-15 21:04:03 -0500

    The Basics

---
+++ The_Basics/movie_years.rb
@@ -0,0 +1,9 @@
+movies = { Good_Fellas: 1985,
+           Anchorman: 2004,
+           The_Godfather: 1972,
+           Pulp_Fiction: 1995 }
+
+puts movies[:Good_Fellas]
+puts movies[:Anchorman]
+puts movies[:The_Godfather]
+puts movies[:Pulp_Fiction]
---
+++ The_Basics/movie_years_array.rb
@@ -0,0 +1,5 @@
+dates = [1985, 2004, 1972, 1995]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
