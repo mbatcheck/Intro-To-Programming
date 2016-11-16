b26b9be47d78a08392db574e68bfe171f86110cd
Mark Batcheck <mark@fourwindsintl.com>
2016-11-16 09:26:18 -0500

    Variables

--- Variables/age.rb
+++ Variables/age.rb
@@ -1,7 +1,16 @@
 # age.rb
 
 puts "So... how old are you, Oldster?"
-age = gets.chomp
-puts "So you are " + age
-puts "In 30 years you will be"
-puts age.to_i + 30
+age = gets.chomp.to_i
+age10 = age + 10
+age20 = age + 20
+age30 = age + 30
+puts "So, you're really only " + age.to_s + "?"
+puts "Well, if that's true..."
+puts "In 10 years you will be: " + age10.to_s
+puts "In 20 years you will be: " + age20.to_s
+puts "In 30 years you will be: " + age30.to_s
+puts "In 40 years you will be..."
+puts "DEAD"
+puts "(probably)"
+puts "(sorry)"
