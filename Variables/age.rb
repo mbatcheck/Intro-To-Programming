09d60d5b23cee57719791e0758f809c774306407
Mark Batcheck <mark@fourwindsintl.com>
2016-11-15 23:25:38 -0500

    latest

---
+++ Variables/age.rb
@@ -0,0 +1,7 @@
+# age.rb
+
+puts "So... how old are you, Oldster?"
+age = gets.chomp
+puts "So you are " + age
+puts "In 30 years you will be"
+puts age.to_i + 30
---
+++ Variables/name.rb
@@ -0,0 +1,5 @@
+# name.rb
+
+puts "Enter your name!"
+name = gets.chomp
+puts "Well, it looks like your name is " + name + "!"
